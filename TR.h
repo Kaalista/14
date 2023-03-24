2fe685c06de160bb3382fa40aa76e840ad657624
Помехи Full HD <114117553+Kaalista@users.noreply.github.com>
2023-03-24 19:16:33 +0300

    Update TR.h

--- TR.h
+++ TR.h
@@ -3,31 +3,22 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 const int ALPHABET_SIZE = 26;
 struct TrieNode
-{
-    struct TrieNode* children[ALPHABET_SIZE];
+{struct TrieNode* children[ALPHABET_SIZE];
     bool OfWord;
-
     TrieNode();
-    ~TrieNode();
-};
-
+    ~TrieNode();};
 class TRIE
-{
-public:
+{public:
     TRIE();
     ~TRIE();
-
     TrieNode* getNewNode();
     void insert(TrieNode* root, const string& key);
     TrieNode* search(const string& key);
     void dictionary();
-  string entPref();
-  vector<string> vectorWords(TrieNode* pNode, string str, vector<string> vec);//
+    string entPref();
+    vector<string> vectorWords(TrieNode* pNode, string str, vector<string> vec);
     void prefix(string pref);
-
 private:
-    TrieNode* root;
-};
+    TrieNode* root;};
